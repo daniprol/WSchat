378bd3540a40035ae7d2a512396970bf9961e3a3
Dani Prol <prolcambados@gmail.com>
2020-12-10 00:08:10 +0100

    Login page and hashing passwords funcitonality added

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 # from wtform_fields import *
 import wtform_fields as wtff
 from flask_sqlalchemy import SQLAlchemy
 from models import User
+from passlib.hash import pbkdf2_sha256
+
 
 app = Flask(__name__)
 # Secret key to sign the cookies in the flask sessions
@@ -28,6 +30,12 @@ def index():
         username = reg_form.username.data
         password = reg_form.password.data
 
+        # We need to generate a hash from the plain text password
+        hashed_password = pbkdf2_sha256.hash(password)
+        # This modules takes care of the salt and the number of iterations (29000 by default!)
+        # If we want to do it manually:
+        # pbkdf2_sha256.using(rounds=1000, salt_size=8).hash(password)
+
         # Check that username is not already in the database:
         # user_object = User.query.filter_by(username=username).first()
 
@@ -35,15 +43,36 @@ def index():
         #     return 'Someone else already has that username!'
 
         # Create a user:
-        user = User(username=username, password=password)
+        user = User(username=username, password=hashed_password)
         db.session.add(user)
         db.session.commit()
 
-        return 'User added to the database successfully!'
+        return redirect(url_for('login'))
     # else:
     #     return 'Validation error!'
 
     return render_template('index.html', form=reg_form)
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+
+    login_form = wtff.LoginForm()
+
+    if login_form.validate_on_submit():
+        # user_object = User.query.filter_by(
+        #     username=login_form.username.data).first()
+        # # IMPORTANT: you need to call .first() or .all() after the query, otherwise you will receive an object that passes the if statement!
+
+        # if not user_object:
+        #     return 'Username or password incorrect. Please, try again!'
+        # elif user_object and user_object.password != login_form.password.data:
+        #     return 'Username or password incorrect. Please, try again!'
+        # else:
+        return 'Login was successful!'
+
+    # We also return this for GET requests!
+    return render_template('login.html', form=login_form)
 
 
 if __name__ == "__main__":
--- wtform_fields.py
+++ wtform_fields.py
@@ -2,8 +2,27 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
 from models import User
+from passlib.hash import pbkdf2_sha256
 
 # We need to give the form a name to define it:
+
+
+def invalid_credentials(form, field):
+    username_introduced = form.username.data
+    # password_introduced = form.password.data
+    # In this case field=password:
+    password_introduced = field.data
+
+    user_object = User.query.filter_by(username=username_introduced).first()
+
+    if user_object is None:
+        raise ValidationError(
+            'Username or password is incorrect. Please try again!')
+    elif pbkdf2_sha256.verify(password_introduced, user_object.password):
+        return
+    else:
+        raise ValidationError(
+            'Username or password is incorrect. Please try again!')
 
 
 class RegistrationForm(FlaskForm):
@@ -38,3 +57,45 @@ class RegistrationForm(FlaskForm):
         if user_object:
             raise ValidationError(
                 'Username already exists! Select a different one!')
+
+
+class LoginForm(FlaskForm):
+    ''' Login Form '''
+
+    # 'username_label' refers to the <Label> tag in the HTML
+    username = StringField('username_label',
+                           validators=[
+                               InputRequired(message='Username is required')]
+                           )
+    # If you want to add a placeholder from here: render_kw={"placeholder": "Username"}
+
+    password = PasswordField('password_label',
+                             validators=[
+                                 InputRequired(message='Password is required'), invalid_credentials]
+                             )
+
+    submit_button = SubmitField('Create')
+
+    # Create a custom validator:
+
+    # def validate_username(self, username):
+    #     user_object = User.query.filter_by(username=username.data).first()
+    #     # Note the we need to user username.data
+
+    #     if not user_object:
+    #         raise ValidationError(
+    #             'Username or password was incorrect. Please try again!')
+
+    # def validate_password(self, password):
+    #     user_object = User.query.filter_by(username=username.data).first()
+    #     # Note the we need to user username.data
+
+    #     if not user_object:
+    #         raise ValidationError(
+    #             'Username or password incorrect. Please, try again!')
+    #     elif user_object and user_object.password != password.data:
+    #         raise ValidationError(
+    #             'Username or password incorrect. Please, try again!')
+    #     else:
+    #         pass
+    # return 'Login was successful!'
